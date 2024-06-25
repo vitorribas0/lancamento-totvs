cdc8f3503e50680fc0f1b3075161ae8453ff9c86
Vitor Silva Ribas de Oliveira <94410917+vitorribas0@users.noreply.github.com>
2024-06-25 13:08:48 -0300

    Update status.py

--- status.py
+++ status.py
@@ -3,6 +3,7 @@ import pandas as pd
 import base64
 import os
 import pickle
+import tempfile
 
 # Função para salvar dados em um arquivo Pickle
 def save_data_to_pickle(data, filename):
@@ -17,8 +18,8 @@ def load_data_from_pickle(filename):
 def main():
     st.title("Leitura de Arquivo .pkl no Streamlit")
 
-    # Nome do arquivo .pkl para armazenar os dados
-    pickle_file = 'dados.pkl'
+    # Nome do arquivo .pkl para armazenar os dados em um diretório temporário
+    pickle_file = os.path.join(tempfile.gettempdir(), 'dados.pkl')
 
     # Sidebar com opções
     menu = ['Carregar Dados', 'Visualizar Dados']
@@ -49,15 +50,18 @@ def main():
 
         # Verifica se o arquivo .pkl existe antes de tentar carregar
         if os.path.exists(pickle_file):
-            loaded_data = load_data_from_pickle(pickle_file)
+            try:
+                loaded_data = load_data_from_pickle(pickle_file)
 
-            # Exibir os dados carregados
-            if isinstance(loaded_data, pd.DataFrame):
-                st.write('**Dados carregados como DataFrame:**')
-                st.write(loaded_data)
-            else:
-                st.write('**Dados carregados:**')
-                st.write(loaded_data)
+                # Exibir os dados carregados
+                if isinstance(loaded_data, pd.DataFrame):
+                    st.write('**Dados carregados como DataFrame:**')
+                    st.write(loaded_data)
+                else:
+                    st.write('**Dados carregados:**')
+                    st.write(loaded_data)
+            except Exception as e:
+                st.error(f"Erro ao carregar o arquivo .pkl: {e}")
         else:
             st.write('Nenhum dado foi carregado ainda.')
 
